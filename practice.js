2ce32ed23df19163000f92a0a9cef90397c2d36c
ttilley02 <ttilley02@gmail.com>
2020-09-06 09:31:27 -0400

    practice

---
+++ practice.js
@@ -0,0 +1,9 @@
+const sortHelpers = require("./setup");
+
+const array1 = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40];
+const array2 = [];
+const array3 = [];
+
+
+const merge1 = sortHelpers.mergeSort(array1)
+console.log(merge1)
